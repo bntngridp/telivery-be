1b2d47afca204950c2d545ed59ac94553fda2bdb
Bintang Ridwan Pribadi <bintangridwan30@gmail.com>
2025-07-30 02:24:36 +0700

    Start project H-9

--- src/modules/auth/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, verifyOtp } from './auth.controller';
-
-const router = Router();
-
-router.post('/register', register);
-router.post('/verify-otp', verifyOtp);
-
-export default router;
